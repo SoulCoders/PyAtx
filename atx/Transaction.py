31604dd2490ff47e10505dad3167f2d9d7cb20c5
tkorays <tkorays@hotmail.com>
2016-10-23 22:31:35 +0800

    完成大部分功能

--- atx/Transaction.py
+++ atx/Transaction.py
@@ -54,7 +54,6 @@ class Command:
 
     @staticmethod
     def __expand_arg(in_arg, delay_arg):
-        delay_arg = [x for x in delay_arg]
         args = {}
         for index, arg in enumerate(in_arg):
             if hasattr(arg, '__call__'):
@@ -63,10 +62,10 @@ class Command:
         return args.values()
 
     def do(self):
-        return self.__do(*self.__expand_arg(self.__do_arg, self.__do_delay_arg))
+        return self.__do(*self.__expand_arg(self.__do_arg, [x for x in self.__do_delay_arg]))
 
     def undo(self):
-        return self.__undo(*self.__expand_arg(self.__undo_arg, self.__undo_delay_arg))
+        return self.__undo(*self.__expand_arg(self.__undo_arg, [x for x in self.__undo_delay_arg]))
 
 
 class Transaction:
@@ -99,20 +98,16 @@ class Transaction:
         self.__commands[len(self.__commands) - 1].set_delay_arg_name(do_arg, undo_arg)
         return self
 
+    # 事务提交,执行成功返回True,失败后自动回滚并返回False
     def commit(self):
         for index, c in self.__commands:
             # 延迟绑定的参数
             da = {}
-            uda = {}
-            do_args = c.get_delay_arg_name()['do']
-            undo_args = c.get_delay_arg_name()['undo']
-            for i, a in enumerate(do_args):
-                # TODO: a存在?
+            for i, a in enumerate(c.get_delay_arg_name()['do']):
+                # TODO: 憋问我为什么不检查是否存在这个键值,懒得检查
                 da[i] = self.__data[a]
-            for i, a in enumerate(undo_args):
-                uda[i] = self.__data[a]
-            # 先获取参数列表,然后获取真实值代替
-            c.set_delay_arg(da.values(), uda.values())
+            c.set_delay_arg(do_arg=da.values())
+
             ret = c.do()
             # 保存返回值
             if c.get_ret()['do']:
@@ -121,130 +116,24 @@ class Transaction:
             # 如果设置了出错后回滚则回滚!
             if not ret and c.is_undo_when_fail():
                 self.rollback(index)
-                break
+                return False
+        return True
 
     def rollback(self, index=-1):
-        pass
-
-
-
-class Command1:
-    def __init__(self, do_func, do_func_args, undo_func, undo_func_args):
-        self.__do_func = do_func
-        self.__do_func_args = do_func_args
-        self.__undo_func = undo_func
-        self.__undo_func_args = undo_func_args
-        self.__return_name = ""
-        self.__do_delay_feed_name = ""
-        self.__undo_delay_feed_name = ""
-        self.__do_delay_args = ()
-        self.__undo_delay_args = ()
-        self.__undo_when_fail = True
-
-    @staticmethod
-    def __expand_args(in_args, delay_args):
-        args = {}
-        for index, arg in enumerate(in_args):
-            if hasattr(arg, '__call__'):
-                args[index] = arg(*delay_args)
-            else:
-                args[index] = arg
-        return args.values()
-
-    def do(self):
-        # we suppose the function to be valid
-        return self.__do_func(*self.__expand_args(self.__do_func_args, self.__do_delay_args))
-
-    def undo(self):
-        return self.__undo_func(*self.__expand_args(self.__undo_func_args, self.__undo_delay_args))
-
-    def set_do_delay_args(self, args):
-        self.__do_delay_args = args
-
-    def set_undo_delay_args(self, args):
-        self.__undo_delay_args = args
-
-    def set_return_name(self, name):
-        self.__return_name = name
-
-    def set_do_delay_feed_name(self, name):
-        self.__do_delay_feed_name = name
-
-    def set_undo_delay_feed_name(self, name):
-        self.__undo_delay_feed_name = name
-
-    def get_return_name(self):
-        return self.__return_name
-
-    def get_do_delay_feed_name(self):
-        return self.__do_delay_feed_name
-
-    def ge_undo_delay_feed_name(self):
-        return self.__undo_delay_feed_name
-
-    def set_undo_when_fail(self, s=True):
-        self.__undo_when_fail = s
-
-
-class Transaction:
-
-    def __init__(self):
-        self.__commands = []
-        self.__data = {}
-        pass
-
-    def append(self, do_func, do_func_args, undo_func, undo_func_args):
-        if not hasattr(do_func, '__call__') or not hasattr(undo_func, '__call__'):
-            logging.error("invalid function!")
-            return self
-        if type(do_func_args) != type(()) or type(undo_func_args) != type(()):
-            logging.error("paramenter should be a tuple!")
-            return self
-        self.__commands.append(Command(do_func, do_func_args, undo_func, undo_func_args))
-        return self
-
-    def ret(self, name):
-        self.__commands[len(self.__commands) - 1].set_return_name(name)
-
-    def feed_do_delay_args(self, name):
-        if name not in self.__data.keys():
-            logging.error("%s is not in the data sets", name)
-            return self
-        self.__commands[len(self.__commands) - 1].set_do_delay_args(self.data[name])
-        return self
-
-    def feed_undo_delay_args(self, name):
-        if name not in self.__data.keys():
-            logging.error("%s is not in the data sets", name)
-            return self
-        self.__commands[len(self.__commands) - 1].set_undo_delay_args(self.data[name])
-        return self
-
-    def rollback_when_fail(self, s=True):
-        self.__commands[len(self.__commands) - 1].set_undo_when_fail(s)
-        return self
-
-    def commit(self):
-        for index,c in enumerate(self.__commands):
-            # feed params
-            if c.get_do_delay_feed_name():
-                c.set_do_delay_args(self.__data[c.get_do_delay_feed_name()])
-            ret = c.do()
-            if not ret:
-                self.rollback(index)
-                return False
-            if c.get_return_name():
-                self.__data[c.get_return_name()] = ret
-        return True
-
-    def rollback(self, start=-1):
-        index = len(self.__commands)-1 if start==-1 else start
-        if index < 0 or index >= len(self.__commands):
-            logging.error("rollback index error!")
-            return False
+        index = index if 0 <= index < len(self.__commands) else (len(self.__commands) - 1)
         while index >= 0:
             c = self.__commands[index]
-            if c.ge_undo_delay_feed_name():
-                c.set_undo_delay_args(self.__data[c.ge_undo_delay_feed_name()])
-            c.undo()
-        return True
+
+            # 延迟参数绑定
+            uda = {}
+            for i, a in enumerate(c.get_delay_arg_name()['undo']):
+                # TODO: 如果你传入一个错误的参数,怪我我咯?
+                uda[i] = self.__data[a]
+            # 此时并不关心给do参数设置成啥
+            c.set_delay_arg(undo_arg=uda.values())
+
+            # 回滚失败继续执行
+            ret = c.undo()
+            if c.get_ret()['undo']:
+                self.__data[c.get_ret()['undo']] = ret
+            index -= 1
