7976205bf1a320b7d9e5fca89fe13b61a202fe49
tkorays <tkorays@hotmail.com>
2016-10-28 19:42:49 +0800

    add comments

--- atx/Shell.py
+++ atx/Shell.py
@@ -5,7 +5,98 @@ Shell
 众生万象,皆有规律可循,神一般的我发现了其中的规律,开始指手画脚。
 我就像个产品狗,我不动手实践,我就瞎BB。
 
-Builder使用组合方式还是类继承由设计者决定。
+Builder使用组合方式还是类继承由设计者决定。这里只是提供了一个解决方案,并没有提供具体功能。
+
+
+总要一个例子来说明下
+-----------------
+from atx.Shell import *
+import time
+
+
+# 我伪装成一个真实shell,不管你发什么,我都只返回时间
+class ShellSimulator:
+    def send(self, cmd=''):
+        return time.ctime()
+
+
+# 通过telnet接入shell
+class TelnetAccessShell(ShellAccess):
+    def __init__(self):
+        self.__conn = ShellSimulator()
+
+    def open(self, address):
+        print("telnet open at ", address)
+        return True
+
+    def close(self):
+        print("telnet close")
+        return True
+
+    def send(self, cmd, exp_list, timeout):
+        print('Telnet send, recv:', self.__conn.send())
+        return 0, None, ''
+
+
+# 通过COM接入shell
+class ComAccessShell(ShellAccess):
+    def __init__(self):
+        self.__conn = ShellSimulator()
+
+    def open(self, address):
+        print("COM open at ", address)
+        return True
+
+    def close(self):
+        print("COM close")
+        return True
+
+    def send(self, cmd, exp_list, timeout):
+        print('COM send, recv:', self.__conn.send())
+        return 0, None, ''
+
+
+# 一个可用的shell,真实环境中可能需要继承或组合
+class MyShell(Shell):
+    def __init__(self, acc=None):
+        self._acc = None
+        Shell.__init__(self, acc)
+
+    def open(self, address):
+        return self._acc.open(address)
+
+    def close(self):
+        return self._acc.close()
+
+    def send(self, cmd, exp_list=None, timeout=5):
+        return self._acc.send(cmd, exp_list, timeout)
+
+    # 可以在这里面实现更多功能
+    # .......
+
+
+class MyShellBuilder(ShellTypeBuilder):
+    def build(self, ac):
+        sh = MyShell(ac)
+        return sh
+
+
+ShellCreator.add('myshell', MyShellBuilder)
+
+mysh = ShellCreator.create('myshell', TelnetAccessShell)
+mysh.open(('127.0.0.1', 1000))
+mysh.send('abc')
+mysh.close()
+
+mysh = ShellCreator.create('myshell', ComAccessShell)
+mysh.open(('COM1', 9600))
+mysh.send('abc')
+mysh.close()
+
+
+关于
+-----
+Author: tkorays<tkorays@hotmail.com>
 
 """
 
