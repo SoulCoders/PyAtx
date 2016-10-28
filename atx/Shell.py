f78fe7aadb8ebfc61285fb7c6be026df8ed7d4bf
tkorays <tkorays@hotmail.com>
2016-10-28 19:03:35 +0800

    添加shell接入层,支持多种接入方式

--- atx/Shell.py
+++ atx/Shell.py
@@ -10,19 +10,39 @@ Builder使用组合方式还是类继承由设计者决定。
 """
 
 
+class ShellAccess:
+    """
+    吾乃Shell之眼
+    """
+    def open(self, address):
+        return False
+
+    def close(self):
+        return False
+
+    def send(self, cmd, exp_list, timeout):
+        return -1, None, ''
+
+
 class Shell:
     """
     出生的时候,我懵懂无知,什么都不懂,父辈们细心地教会我一切;
     待我及笄,你看到的将是新的我。
     """
-    pass
+    def __init__(self):
+        self.__acc = None
+
+    def __set_access__(self, ac):
+        a = ac()
+        if isinstance(a, ShellAccess):
+            self.__acc = a
 
 
 class ShellTypeBuilder:
     """
     我自己也啥都不干,我的子孙沿着我走过的轨迹探索未知;
     """
-    def build(self):
+    def build(self, ac):
         return None
 
 
@@ -36,11 +56,11 @@ class ShellCreator:
 
     # 我说,要有鲍鱼,厨师就端上来了
     @staticmethod
-    def create(name):
+    def create(name, ac):
         for sh in ShellCreator.__shells:
             if sh[0] == name:
-                return sh[1]().build()
-        return ShellTypeBuilder().build()
+                return sh[1]().build(ac)
+        return ShellTypeBuilder().build(ac)
 
     # 我喜欢不断地请厨师为我烹饪美食
     @staticmethod
