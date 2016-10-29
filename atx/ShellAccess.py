3a435fad4993eabc8f679323cdab142b74da28cf
tkorays <tkorays@hotmail.com>
2016-10-29 21:11:38 +0800

    添加了shell接入层框架

---
+++ atx/ShellAccess.py
@@ -0,0 +1,53 @@
+import telnetlib
+
+
+class ShellAccess:
+    def __init__(self, newline='\n'):
+        self._newline = newline
+
+    def open(self, address):
+        pass
+
+    def close(self):
+        pass
+
+    def send(self, cmd, exp=None, timeout=5, newline=True):
+        pass
+
+    def read(self, exp, timeout=5):
+        pass
+
+
+class TelnetShell(ShellAccess):
+    def __init__(self, newline='\n'):
+        ShellAccess.__init__(self, newline)
+        self._conn = telnetlib.Telnet()
+
+    def open(self, address):
+        self._conn.open(*address)
+        return True
+
+    def close(self):
+        self._conn.close()
+        return True
+
+    def send(self, cmd, exp=None, timeout=5, newline=True):
+        pass
+
+    def read(self, exp, timeout=5):
+        pass
+
+
+class TcpShell(ShellAccess):
+    def __init__(self, newline='\n'):
+        ShellAccess.__init__(self, newline)
+
+
+class SerialShell(ShellAccess):
+    def __init__(self, newline='\n'):
+        ShellAccess.__init__(self, newline)
+
+
+class SshShell(ShellAccess):
+    def __init__(self, newline='\n'):
+        ShellAccess.__init__(self, newline)
